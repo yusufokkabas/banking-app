ad38b88909138284e9acdc3212fd5f809e19fce3
Yusuf Orhan Okkabas <yusuf.okkkabas@kentkart.com>
2024-09-23 13:43:29 +0300

    initial commit; route groups and initial settings are created

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <main>
+        SIDEBAR
+        {children}
+      </main>
+    </html>
+  );
+}
