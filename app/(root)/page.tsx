0d7338e32352423aaeebac37d47f97daf98f7e6b
Yusuf Orhan <yusufokkabas@gmail.com>
2024-09-29 11:57:02 +0300

    feat: create home page with complete layout, define route

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-
+import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 const Home = () => {
-  const loggedIn = { firstName: 'Yusuf' }
+  const loggedIn = {
+    firstName: "Yusuf",
+    lastName: "Okkabas",
+    email: "yusufokkabas@gmail.com",
+  };
   return (
     <section className="home">
-      <div className="home-content"> 
+      <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
-          type="greeting"
-          title= "Welcome"
-          user= {loggedIn?.firstName || 'Guest' } 
-          subtext="Access and manage your account and transactions efficiently."
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
           ></HeaderBox>
-        </header>
-        <TotalBalanceBox 
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1234}
-        />
-      </div>
-    </section>
-  )
-}
 
-export default Home
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1234}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 12345 }, { currentBalance: 1452 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
